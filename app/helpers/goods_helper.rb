5eb6ff2c56e1914c4e3d24b109a44c69fe5a28d5
Mario Avila <avilamario16@gmail.com>
2016-02-23 14:58:28 -0600

    Used the relationship of Good-GoodType in Good views

--- app/helpers/goods_helper.rb
+++ app/helpers/goods_helper.rb
@@ -1,2 +1,14 @@
 module GoodsHelper
+  def good_types_select
+
+    types = GoodType.all
+      final_types = []
+
+      types.each do |t|
+        final_types << [t.name, t.id]
+
+      end
+      final_types
+  end
+
 end
